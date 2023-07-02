053d044edaeb81e49c7e79b6f01894eeb297945f
serdekh <mistersergey055@gmail.com>
2023-07-02 14:53:14 +0200

    Add project files.

---
+++ lesson16/ConsoleMessager.cs
@@ -0,0 +1,30 @@
+﻿/// <summary>
+/// Prints different kinds of messages in the console
+/// </summary>
+public class ConsoleMessager
+{
+    /// <summary>
+    /// Prints a message with a certain color
+    /// </summary>
+    /// <param name="message">a message to print</param>
+    /// <param name="color">message color</param>
+    public static void WriteMessage(string message, ConsoleColor color)
+    {
+        var initialColor = Console.ForegroundColor;
+        Console.ForegroundColor = color;
+        Console.WriteLine(message);
+        Console.ForegroundColor = initialColor;
+    }
+
+    /// <summary>
+    /// Prints "Error: " + an error message in red
+    /// </summary>
+    /// <param name="error">error message to print</param>
+    public static void WriteError(string error) => WriteMessage("Error: " + error, ConsoleColor.Red);
+    
+    /// <summary>
+    /// Prints "Success: " + a success message in green
+    /// </summary>
+    /// <param name="message">success message to print</param>
+    public static void WriteSuccess(string message) => WriteMessage("Success: " + message, ConsoleColor.Green);
+}
---
+++ lesson16/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        FileTimer.PrintWelcomeMessage();
+        FileTimer.PrintOptions();
+
+        var fileTimer = new FileTimer();
+        
+        while (true)
+        {
+            fileTimer.ReadLine();
+        }    
+    }
+}
